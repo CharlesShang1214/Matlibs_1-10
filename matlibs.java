2264160852b860fff30daaceabdeb70028496d5a
CharlesShang1214 <student.shang27@student.tamdistrict.org>
2024-09-26 10:08:57 -0700

    first draft of the scanner class

--- matlibs.java
+++ matlibs.java
@@ -3,18 +3,13 @@ import java.util.Scanner;
 public class matlibs{
     public static void main(String[] args) {
         Scanner scanner= new Scanner(System.in);
-        System.out.println("Give me a noun");
-        String noun=scanner.nextLine();
-        System.out.println("Give me a plural noun");
-        String pNoun=scanner.nextLine();
-        System.out.println("Give me a number");
-        String number=scanner.nextLine();
-        System.out.println("Give me a adj");
-        String adj=scanner.nextLine();
-        System.out.println("Give me a adv");
-        String adv=scanner.nextLine();
-        System.out.println("I went to the animal "+noun+", the "+pNoun+" and the "+number+ " beasts were there.");
-        
+        String madlib="I went to the animal <noun>, the <plural_noun> and the <number> beasts were there." ;
+        String betweenSqBrac=madlib.substring(madlib.indexOf("<")+1,madlib.indexOf(">"));
+        if (betweenSqBrac=="noun"){
+            System.out.println("Please give me a noun");
+            String noun=scanner.nextLine();
+        }
+
 
     }
 }
