588e553327454acd4adaf55ed571a39d367f8371
CharlesShang1214 <student.shang27@student.tamdistrict.org>
2024-09-30 09:10:48 -0700

    better logic, capeble of longer madlibs

--- matlibs.java
+++ matlibs.java
@@ -3,32 +3,40 @@ import java.util.Scanner;
 public class matlibs{
     public static void main(String[] args) {
         Scanner scanner= new Scanner(System.in);
-                
-      
+
+
         while (true) {
-            
-        
+
+
         String madlib;
         while (true) {
             System.out.println("Which madlib you want?(one-two-three)");
             String matlibNum= scanner.nextLine();
             if (matlibNum.compareTo("one")==0) {
-            madlib="I went to the animal <noun>, the <plural_noun> and the <number> beasts were there." ; 
+            madlib="I went to the animal <noun>, the <plural_noun> and the <number> beasts were there." ;
             break;
             }
-            
+
             if (matlibNum.compareTo("two")==0) {
                 madlib="Once upon a time in a <adjective> land, a <noun> <adverb> grew.";
                 break;
             }
             if (matlibNum.compareTo("three")==0) {
-                madlib="I <verb> to the <animal>, and it called me <famous person>.";
+                madlib="The <adjective> <animal> decided to <verb> all day, but <different adjective> <different animal> <different verb> it.";
                 break;
             }
             else{
                 System.out.println("Please input one, two, or three, all lowercase.(Has to be words!)");
             }
         }
+        while (madlib.indexOf("<")!=-1) {
+        //https://www.w3schools.com/java/java_operators.asp
+        String BSB=madlib.substring(madlib.indexOf("<"),madlib.indexOf(">")+1);
+        System.out.println("Please give me a "+BSB);
+        String word= scanner.nextLine();
+        madlib=madlib.replace(BSB,word);
+}
+        System.out.println(madlib);
         // this is method one that what probably ap grader would want
         // String BSB1=madlib.substring(madlib.indexOf("<"),madlib.indexOf(">")+1);
         // System.out.println("Please give me a "+BSB1);
@@ -57,9 +65,5 @@ public class matlibs{
             System.out.println("Sure, lets do it again");
         }
 
-    }
-
-
-
-    }
-}
+        }
+    }}
